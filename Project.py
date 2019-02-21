87fecdeaf4f39a044b001dc0331e78a5cda12f5d
danth <danthonyjjones@gmail.com>
2019-02-20 19:52:54 -0700

    final

---
+++ Project.py
@@ -0,0 +1,25 @@
+a = 70
+b = 30
+c = a + 3 + 2 * b
+print('hiiiii')
+l = ['numbers'
+     '1'
+     '2'
+     '3'
+     '4'
+     '5'
+     '']
+string = 'games'
+print(string)
+for l in range(2):
+    print(b)
+i = 8
+while i >= 0:
+    print(i)
+    i = i - 1
+
+    t = 8
+while True:
+    if t <= 0:
+        break
+    t = t - 1
